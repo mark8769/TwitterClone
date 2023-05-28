27be80a29a454e10ef9cc580eef1f54bc623c940
Mark Ortega <mortegaponce27@gmail.com>
2023-05-27 21:51:09 -0500

    network/models.py: Added Post and Like models for social media site.

--- network/models.py
+++ network/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        s = f"User: {self.user}"
+        s += f"time: {self.datetime}"
+
+        return s
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        s = f"User: {self.user}"
+        s += f"Post: {self.post}"
+
+        return s
+
+# class Following(models.Model):
+#     
+#     have to have related names since Django won't know which user to refer to which of next two variables.s
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     user_being_followed = models.ForeignKey(User, on_delete=models.CASCADE)
