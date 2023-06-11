905114081f9eccf23d4821e3f6bf488df51e2a8f
Mark Ortega <mortegaponce27@gmail.com>
2023-06-10 20:38:04 -0500

    network/admin.py: Registered Following class model onto Django Admin portal.

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Post, Like
+from .models import User, Post, Like, Following
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Like)
+admin.site.register(Following)
